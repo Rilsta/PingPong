17cd15ee5d1e1c8063f0fd984cb217f98d8fdb1a
unknown <rstarnes9@gmail.com>
2015-11-09 11:31:10 -0800

    Run first tests for number counting

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var pingPong = function(pipoNumber){
+  for (var index = 1; index <= pipoNumber; index += 1)
+    return(index);
+  };
---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+describe("pingPong", function(){
+
+  it("counts up from 1 to the number inputted", function(){
+    expect(pingPong(3)).to.equal(1,2,3);
+  });
+
+});
