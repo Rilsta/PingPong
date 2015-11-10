5826773d18d08c87886523aac275684db42c1a61
unknown <rstarnes9@gmail.com>
2015-11-10 09:56:52 -0800

    Pass first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-var pingPong = function(pipoNumber){
-  for (var index = 1; index <= pipoNumber; index += 1)
-    return(index);
-  };
+var pingPong = function(pingPongInput){
+
+var myArray = []
+
+  for (var index = 1; index <= pingPongInput; index += 1)
+    myArray.push(index)
+      return myArray
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
 describe("pingPong", function(){
 
   it("counts up from 1 to the number inputted", function(){
-    expect(pingPong(3)).to.equal(1,2,3);
+    expect(pingPong(5)).to.eql([ 1, 2, 3, 4, 5 ]);
   });
 
 });
