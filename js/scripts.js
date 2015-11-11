ae012c622b712ed6beb3ef46ee77cdfc477e5728
Rilsta <rstarnes9@gmail.com>
2015-11-10 17:04:51 -0800

    Add test for divisible by 15.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ var myArrays = []
 
   for (var index = 1; index <= pingPongInput; index += 1)
     if ((index % 3) === 0){
-        myArrays.push(0);
-      } 
-        else {
-          myArrays.push(index)
+        myArrays.push("ping");
+      }
+        else if ((index % 5) === 0){
+          myArrays.push("pong");
         }
-            return myArrays
 
-
+          else {
+            myArrays.push(index)
+          }
+              return myArrays
 
 };
--- spec/specs.js
+++ spec/specs.js
@@ -4,8 +4,16 @@ describe("pingPong", function(){
     expect(pingPong(5)).to.eql([ 1, 2, 3, 4, 5 ]);
   });
 
-  it("replace all numbers divisible by 3 with 0", function(){
-    expect(pingPong(6)).to.eql([ 1, 2, 0, 4, 5, 0 ])
+  it("replace all numbers divisible by 3 with ping", function(){
+    expect(pingPong(6)).to.eql([ 1, 2, "ping", 4, 5, "ping" ])
+  });
+
+  it("replace all number divisible by 5 with pong", function(){
+    expect(pingPong(10)).to.eql([ 1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong" ])
+  });
+
+  it("replace all numbers divisible by 15 with pingpong", function(){
+    expect(pingPong(15)).to.eql([ 1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong" ])
   });
 
 });
